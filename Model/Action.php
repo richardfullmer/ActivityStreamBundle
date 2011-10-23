c06a0493825cf9aa7aa598ce28228523ce933bab
Richard Fullmer <richardfullmer@gmail.com>
2011-10-23 12:11:41 -0700

    Make action check for doctrine proxies on class detection for actor, target and action objects

--- Model/Action.php
+++ Model/Action.php
@@ -127,7 +127,14 @@ abstract class Action implements ActionInterface
     public function setActor($actor)
     {
         $this->actorId = $actor->getId();
-        $this->actorType = get_class($actor);
+
+        $type = get_class($actor);
+        $proxyClass = new \ReflectionClass($type);
+        if ($proxyClass->implementsInterface('Doctrine\ORM\Proxy\Proxy')) {
+            $type = $proxyClass->getParentClass()->getName();
+        }
+
+        $this->actorType = $type;
         $this->actor = $actor;
     }
 
@@ -149,7 +156,14 @@ abstract class Action implements ActionInterface
     public function setTarget($target)
     {
         $this->targetId = $target->getId();
-        $this->targetType = get_class($target);
+
+        $type = get_class($target);
+        $proxyClass = new \ReflectionClass($type);
+        if ($proxyClass->implementsInterface('Doctrine\ORM\Proxy\Proxy')) {
+            $type = $proxyClass->getParentClass()->getName();
+        }
+
+        $this->targetType = $type;
         $this->target = $target;
     }
     
@@ -166,7 +180,14 @@ abstract class Action implements ActionInterface
     public function setActionObject($actionObject)
     {
         $this->actionObjectId = $actionObject->getId();
-        $this->actionObjectType = get_class($actionObject);
+
+        $type = get_class($actionObject);
+        $proxyClass = new \ReflectionClass($type);
+        if ($proxyClass->implementsInterface('Doctrine\ORM\Proxy\Proxy')) {
+            $type = $proxyClass->getParentClass()->getName();
+        }
+
+        $this->actionObjectType = $type;
         $this->actionObject = $actionObject;
     }
     
